41904adf286ab7c4cfd7d6ce2c22e54796b44204
Divyanshu Semwal <dlsemwal@gmail.com>
2020-04-01 20:48:38 +0530

    fixed file address

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(__dirname + '/dist/simple-hr'))
 
 app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/index.html')
+  res.status(200).sendFile(__dirname + '/dist/simple-hr/index.html')
 })
 
 app.listen(process.env.PORT || 8080)
